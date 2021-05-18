48d0dfd21c838ede7b8378666f1bc7485e513daf
Priyank <priyupadhyay1994@gmail.com>
2021-05-19 00:40:03 +0530

    port modification

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-export const { ENV, PORT, FIREBASE_DATABASE_URL } = process.env;
+export const { NODE_ENV, NODE_PORT, FIREBASE_DATABASE_URL } = process.env;
