e2a30878a0c9211ac40e6ecd827b1123de5ce038
Priyank Upadhyay <priyupadhyay1994@gmail.com>
2021-05-19 09:52:27 +0530

    Update firebase_admin_config.js

--- src/services/firebase/firebase_admin_config.js
+++ src/services/firebase/firebase_admin_config.js
@@ -1,9 +1,6 @@
 import firebase from 'firebase-admin';
 import { FIREBASE_DATABASE_URL } from '../../settings';
 
-const serviceAccount = require('./firebase_permission.json');
-
 export default firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
   databaseURL: FIREBASE_DATABASE_URL
 });
